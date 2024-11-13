710da02df9ec51d53f1cadb346376190e77ad475
Figuur <lmulder_89@hotmail.nl>
2024-11-13 11:02:53 +0100

    currency converter

---
+++ script.js
@@ -0,0 +1,14 @@
+// function gameZoeken() {
+//     fetch("https://www.freetogame.com/api/games?platform=pc")
+//         .then((response) => response.json())
+//         .then((data) => gameVinden(data[0]))
+//         .catch((error) => console.error("Error fetching data:", error));
+// }
+
+// function gameVinden(data) {
+//     document.getElementById("gametitel").innerHTML = data.title;
+// }
+
+// window.onload = function () {
+//     gameZoeken();
+// };
